2fb22cb5e9fc26eee9fe4fb8f5cd381db3328c6f
spiotrpg-dot <spiotrpg@gmail.com>
2025-11-07 11:53:25 +0100

    Add files via upload

---
+++ js/bricklink-colors.js
@@ -0,0 +1,924 @@
+let ALL_BRICKLINK_SOLID_COLORS = [
+   
+
+
+    {
+        name: "Medium Lilac",
+        hex: "#3f3691",
+        id: 89,
+    },
+    {
+        name: "Medium Lavender",
+        hex: "#ac78ba",
+        id: 157,
+    },
+    {
+        name: "Dark Green",
+        hex: "#00852b",
+        id: 80,
+    },
+    {
+        name: "Vibrant Yellow",
+        hex: "#ebd800",
+        id: 236,
+    }, 
+    {
+        name: "Naugat",
+        hex: "#d09168",
+        id: 28,
+    },
+    {
+        name: "Cool Yellow",
+        hex: "#ffec6c",
+        id: 103,
+    },
+    {
+        name: "Spring Yellowish Green",
+        hex: "#dfeea5",
+        id: 158,
+    },
+    {
+        name: "Light Purple",
+        hex: "#e4adc8",
+        id: 93,
+    },
+    {
+        name: "Bright Yellowish Green",
+        hex: "#a5ca18",
+        id: 34,
+    },
+    {
+        name: "Sand Blue",
+        hex: "#70819a",
+        id: 55,
+    },
+    {
+        name: "Earth Green",
+        hex: "#00451a",
+        id: 80,
+    },
+    {
+        name: "Sand Yellow",
+        hex: "#958a73",
+        id: 69,
+    },
+    {
+        name: "Dark Orange",
+        hex: "#a95500",
+        id: 68,
+    },
+    {
+        name: "Dark Azur",
+        hex: "#078bc9",
+        id: 153,
+    },
+    {
+        name: "Flame Yellowish Orange",
+        hex: "#f8bb3d",
+        id: 110,
+    },
+    {
+        name: "Bright Purple",
+        hex: "#c870a0",
+        id: 47,
+    },
+    {
+        name: "Lavender",
+        hex: "#e1d5ed",
+        id: 154,
+    },
+    {
+        name: "Aqua",
+        hex: "#b3d7d1",
+        id: 41,
+    },
+    {
+        name: "Vibrant Coral",
+        hex: "#ff698f",
+        id: 220,
+    },
+    {
+        name: "Bright Green",
+        hex: "#4b9f4a",
+        id: 36,
+    },
+    {
+        name: "Medium Azure",
+        hex: "#36aebf",
+        id: 156,
+    },
+    {
+        name: "Bright Reddish Violet",
+        hex: "#923978",
+        id: 71,
+    },
+    {
+        name: "Dark Brown",
+        hex: "#352100",
+        id: 120,
+    },
+    {
+        name: "Bright Bluish Green",
+        hex: "#008f9b",
+        id: 39,
+    },
+    {
+        name: "Bright Orange",
+        hex: "#fe8a18",
+        id: 4,
+    },
+    {
+        name: "Bright Blue",
+        hex: "#0055bf",
+        id: 7,
+    },
+    {
+        name: "Light Nougat",
+        hex: "#ffc995",
+        id: 90,
+    },
+    {
+        name: "Dark Stone Grey",
+        hex: "#6c6e68",
+        id: 85,
+    },
+    {
+        name: "Medium Nougat",
+        hex: "#aa7d55",
+        id: 150,
+    },
+    {
+        name: "Reddish Brown",
+        hex: "#582a12",
+        id: 88,
+    },
+    {
+        name: "Dark Red",
+        hex: "#720e0f",
+        id: 59,
+    },
+    {
+        name: "Bright Yellow",
+        hex: "#f2cd37",
+        id: 3,
+    },
+    {
+        name: "Bright Red",
+        hex: "#c91a09",
+        id: 5,
+    },
+    {
+        name: "Brick Yellow",
+        hex: "#e4cd9e",
+        id: 2,
+    },
+    {
+        name: "White",
+        hex: "#ffffff",
+        id: 1,
+    },
+    {
+        name: "Black",
+        hex: "#05131d",
+        id: 11,
+    },
+    {
+        name: "Olive Green",
+        hex: "#9b9a5a",
+        id: 155,
+    },
+    {
+        name: "Earth Blue",
+        hex: "#0a3463",
+        id: 63,
+    },
+    {
+        name: "Medium Stone Grey",
+        hex: "#a0a5a9",
+        id: 86,
+    },
+
+
+
+   /** {
+        name: "Very Light Gray",
+        hex: "#e8e8e8",
+        id: 49,
+    },
+    {
+        name: "Very Light Bluish Gray",
+        hex: "#e4e8e8",
+        id: 99,
+    },
+   
+    {
+        name: "Light Gray",
+        hex: "#9c9c9c",
+        id: 9,
+    },
+    {
+        name: "Dark Gray",
+        hex: "#6b5a5a",
+        id: 10,
+    },
+    
+   
+    
+    {
+        name: "Red",
+        hex: "#b30006",
+        id: 5,
+    },
+    {
+        name: "Rust",
+        hex: "#b52c20",
+        id: 27,
+    },
+    {
+        name: "Coral",
+        hex: "#f88379",
+        id: 220,
+    },
+    {
+        name: "Salmon",
+        hex: "#f45c40",
+        id: 25,
+    },
+    {
+        name: "Light Salmon",
+        hex: "#ffdedc",
+        id: 26,
+    },
+    {
+        name: "Sand Red",
+        hex: "#8c6b6b",
+        id: 58,
+    },
+    
+    {
+        name: "Brown",
+        hex: "#532115",
+        id: 8,
+    },
+   
+    {
+        name: "Dark Tan",
+        hex: "#907450",
+        id: 69,
+    },
+    {
+        name: "Tan",
+        hex: "#dec69c",
+        id: 2,
+    },
+    {
+        name: "Nougat",
+        hex: "#ffaf7d",
+        id: 28,
+    },
+   
+    {
+        name: "Dark Nougat",
+        hex: "#e78b3e",
+        id: 225,
+    },
+    {
+        name: "Medium Brown",
+        hex: "#a16c42",
+        id: 91,
+    },
+    {
+        name: "Fabuland Brown",
+        hex: "#b3694e",
+        id: 106,
+    },
+    {
+        name: "Fabuland Orange",
+        hex: "#ef9121",
+        id: 160,
+    },
+    {
+        name: "Earth Orange",
+        hex: "#e6881d",
+        id: 29,
+    },
+    {
+        name: "Dark Orange",
+        hex: "#b35408",
+        id: 68,
+    },
+    {
+        name: "Neon Orange",
+        hex: "#fa5947",
+        id: 165,
+    },
+    {
+        name: "Orange",
+        hex: "#ff7e14",
+        id: 4,
+    },
+    {
+        name: "Medium Orange",
+        hex: "#ffa531",
+        id: 31,
+    },
+    {
+        name: "Bright Light Orange",
+        hex: "#f7ba30",
+        id: 110,
+    },
+    {
+        name: "Light Orange",
+        hex: "#f7ad63",
+        id: 32,
+    },
+    {
+        name: "Very Light Orange",
+        hex: "#e6c05d",
+        id: 96,
+    },
+    {
+        name: "Dark Yellow",
+        hex: "#dd982e",
+        id: 161,
+    },
+    {
+        name: "Yellow",
+        hex: "#f7d117",
+        id: 3,
+    },
+    {
+        name: "Bright Light Yellow",
+        hex: "#f3e055",
+        id: 103,
+    },
+    {
+        name: "Light Yellow",
+        hex: "#ffe383",
+        id: 33,
+    },
+    {
+        name: "Light Lime",
+        hex: "#ebee8f",
+        id: 35,
+    },
+    {
+        name: "Yellowish Green",
+        hex: "#dfeea5",
+        id: 158,
+    },
+    {
+        name: "Neon Green",
+        hex: "#bcef66",
+        id: 166,
+    },
+    {
+        name: "Medium Lime",
+        hex: "#bdc618",
+        id: 76,
+    },
+    
+    {
+        name: "Lime",
+        hex: "#a6ca55",
+        id: 34,
+    },
+    
+    {
+        name: "Dark Green",
+        hex: "#2e5543",
+        id: 80,
+    },
+    {
+        name: "Green",
+        hex: "#00642e",
+        id: 6,
+    },
+    {
+        name: "Medium Green",
+        hex: "#62f58e",
+        id: 37,
+    },
+    {
+        name: "Light Green",
+        hex: "#a5dbb5",
+        id: 38,
+    },
+    {
+        name: "Sand Green",
+        hex: "#76a290",
+        id: 48,
+    },
+    {
+        name: "Dark Turquoise",
+        hex: "#008a80",
+        id: 39,
+    },
+    {
+        name: "Light Turquoise",
+        hex: "#31b5ca",
+        id: 40,
+    },
+    {
+        name: "Light Aqua",
+        hex: "#ccffff",
+        id: 152,
+    },
+    {
+        name: "Dark Blue",
+        hex: "#143044",
+        id: 63,
+    },
+    {
+        name: "Blue",
+        hex: "#0057a6",
+        id: 7,
+    },
+    {
+        name: "Dark Azure",
+        hex: "#3399ff",
+        id: 153,
+    },
+    {
+        name: "Medium Blue",
+        hex: "#61afff",
+        id: 42,
+    },
+    {
+        name: "Maersk Blue",
+        hex: "#6badd6",
+        id: 72,
+    },
+    {
+        name: "Bright Light Blue",
+        hex: "#9fc3e9",
+        id: 105,
+    },
+    {
+        name: "Light Blue",
+        hex: "#b4d2e3",
+        id: 62,
+    },
+    {
+        name: "Sky Blue",
+        hex: "#7dbfdd",
+        id: 87,
+    },
+    {
+        name: "Sand Blue",
+        hex: "#5a7184",
+        id: 55,
+    },
+    {
+        name: "Blue-Violet",
+        hex: "#506cef",
+        id: 97,
+    },
+    {
+        name: "Dark Blue-Violet",
+        hex: "#2032b0",
+        id: 109,
+    },
+    {
+        name: "Violet",
+        hex: "#3448a4",
+        id: 43,
+    },
+    {
+        name: "Medium Violet",
+        hex: "#9391e4",
+        id: 73,
+    },
+    {
+        name: "Light Violet",
+        hex: "#c9cae2",
+        id: 44,
+    },
+    {
+        name: "Dark Purple",
+        hex: "#5f2683",
+        id: 89,
+    },
+    {
+        name: "Purple",
+        hex: "#a5499c",
+        id: 24,
+    },
+    {
+        name: "Light Purple",
+        hex: "#da70d6",
+        id: 93,
+    },
+    
+    {
+        name: "Clikits Lavender",
+        hex: "#e0aad9",
+        id: 227,
+    },
+    {
+        name: "Lavender",
+        hex: "#b18cbf",
+        id: 154,
+    },
+    {
+        name: "Sand Purple",
+        hex: "#b57da5",
+        id: 54,
+    },
+    {
+        name: "Magenta",
+        hex: "#b52952",
+        id: 71,
+    },
+    {
+        name: "Dark Pink",
+        hex: "#c87080",
+        id: 47,
+    },
+    {
+        name: "Medium Dark Pink",
+        hex: "#f785b1",
+        id: 94,
+    },
+    {
+        name: "Bright Pink",
+        hex: "#ffbbff",
+        id: 104,
+    },
+    {
+        name: "Pink",
+        hex: "#ffc0cb",
+        id: 23,
+    },
+    {
+        name: "Light Pink",
+        hex: "#ffe1ff",
+        id: 56,
+    },
+    {
+        name: "Neon Yellow",
+        hex: "#fffc00",
+        id: 236,
+    },
+    // add in some pearl colors to support specific sets,
+    // but avoid the other pearl colors for now since they
+    // may weirdly replace similar solid non pearl
+    {
+        name: "Pearl Gold",
+        hex: "#e79500",
+        id: 115,
+    },
+    {
+        name: "Pearl Dark Gray",
+        hex: "#666660",
+        id: 77,
+    },
+    {
+        name: "Flat Silver",
+        hex: "#898788",
+        id: 95,
+    },
+    // The 2 below are new in 2022
+    // {
+    //     name: "Medium Brown",
+    //     hex: "#A16C42",
+    //     id: 240,
+    // },
+    {
+        name: "Medium Tan",
+        hex: "#D9C594",
+        id: 241,
+    },
+
+    */
+];
+
+const HEX_TO_COLOR_NAME = {};
+ALL_BRICKLINK_SOLID_COLORS.forEach((color) => {
+    HEX_TO_COLOR_NAME[color.hex] = color.name;
+});
+
+const COLOR_NAME_TO_ID = {};
+ALL_BRICKLINK_SOLID_COLORS.forEach((color) => {
+    COLOR_NAME_TO_ID[color.name] = color.id;
+});
+
+const KNOWN_BRICKLINK_STUD_COLOR_NAMES = [
+    /**
+    "Black",
+    "Blue",
+    "Bright Green",
+    "Bright Light Yellow",
+    "Bright Pink",
+    "Brown",
+    "Dark Blue",
+    "Dark Bluish Gray",
+    "Dark Brown",
+    "Dark Gray",
+    "Dark Orange",
+    "Dark Pink",
+    "Dark Purple",
+    "Dark Red",
+    "Dark Tan",
+    "Green",
+    "Light Bluish Gray",
+    "Light Gray",
+    "Lime",
+    "Magenta",
+    "Medium Azure",
+    "Medium Lime",
+    "Medium Nougat",
+    "Olive Green",
+    "Orange",
+    "Red",
+    "Reddish Brown",
+    "Sand Blue",
+    "Sand Green",
+    "Tan",
+    "White",
+    "Yellow",
+    "Yellowish Green",
+    "Trans-Black",
+    "Trans-Bright Green",
+    "Trans-Clear",
+    "Trans-Dark Blue",
+    "Trans-Dark Pink",
+    "Trans-Green",
+    "Trans-Light Blue",
+    "Trans-Medium Blue",
+    "Trans-Neon Green",
+    "Trans-Neon Orange",
+    "Trans-Neon Yellow",
+    "Trans-Orange",
+    "Trans-Purple",
+    "Trans-Red",
+    "Trans-Yellow",
+    "Chrome Antique Brass",
+    "Chrome Gold",
+    "Chrome Silver",
+    "Flat Silver",
+    "Pearl Dark Gray",
+    "Pearl Gold",
+    "Pearl Light Gray",
+    "Metallic Silver",
+    "Glow In Dark Opaque",
+    "Glow In Dark White",
+    "Medium Lavender", // manually added from Batman
+    "Light Aqua", // manually added from Batman
+    "Medium Lavender",
+    "Cool Yellow",
+    "Spring Yellowish Green",
+    "Light Purple",
+    "Bright Yellowish Green",
+    "Sand Blue",
+    "Sand Yellow",
+    "Dark Orange",
+    "Dark Azur",
+    "Flame Yellowish Orange",
+    "Bright Purple",
+    "Vibrant Coral",
+    "Bright Reddish Violet",
+    "Bright Bluish Green",
+    "Bright Orange",
+    "Bright Blue",
+    "Dark Stone Grey",
+    "Bright Yellow",
+    "Bright Red",
+    "Brick Yellow",
+    "Earth Blue",
+    "Medium Stone Grey",
+    */
+        "Medium Lilac",
+        "Medium Lavender",
+        "Dark Green",
+        "Vibrant Yellow",
+        "Naugat",
+        "Cool Yellow",
+        "Spring Yellowish Green",
+        "Light Purple",
+        "Bright Yellowish Green",
+        "Sand Blue",
+        "Sand Yellow",
+        "Dark Orange",
+        "Dark Azur",
+        "Flame Yellowish Orange",
+        "Bright Purple",
+        "Lavender",
+        "Aqua",
+        "Vibrant Coral",
+        "Bright Green",
+        "Medium Azure",
+        "Bright Reddish Violet",
+        "Dark Brown",
+        "Bright Bluish Green",
+        "Bright Orange",
+        "Bright Blue",
+        "Light Nougat",
+        "Dark Stone Grey",
+        "Medium Nougat",
+        "Reddish Brown",
+        "Dark Red",
+        "Bright Yellow",
+        "Bright Red",
+        "Brick Yellow",
+        "White",
+        "Black",
+        "Olive Green",
+        "Earth Blue",
+        "Medium Stone Grey",
+        "Earth Green",
+];
+
+const KNOWN_BRICKLINK_TILE_COLOR_NAMES = [
+   /** "Black",
+    "Blue",
+    "Bright Light Blue",
+    "Bright Light Orange",
+    "Bright Pink",
+    "Coral",
+    "Dark Blue",
+    "Dark Bluish Gray",
+    "Dark Brown",
+    "Dark Orange",
+    "Dark Red",
+    "Dark Tan",
+    "Lavender",
+    "Light Bluish Gray",
+    "Light Nougat",
+    "Lime",
+    "Medium Blue",
+    "Medium Nougat",
+    "Nougat",
+    "Olive Green",
+    "Orange",
+    "Red",
+    "Reddish Brown",
+    "Sand Blue",
+    "Tan",
+    "White",
+    "Yellow",
+    "Trans-Black",
+    "Trans-Bright Green",
+    "Trans-Clear",
+    "Trans-Dark Blue",
+    "Trans-Dark Pink",
+    "Trans-Green",
+    "Trans-Light Blue",
+    "Trans-Orange",
+    "Trans-Purple",
+    "Trans-Red",
+    "Trans-Yellow",
+    "Flat Silver",
+    "Pearl Gold",
+    "Satin Trans-Light Blue",
+    "Satin White",
+    "Glow In Dark White",
+    "Glitter Trans-Clear",
+    "Glitter Trans-Dark Pink",
+    "Glitter Trans-Light Blue",
+    "Dark Bluish Gray", // manually added from Warhol
+    "Dark Pink", // manually added from Warhol
+    "Magenta", // manually added from Warhol
+    "Medium Azure", // manually added from Warhol
+    "Neon Yellow", // manually added from dots
+    "Dark Azure", // manually added from art project
+    "Light Aqua", // manually added from art project
+    "Bright Light Yellow", // manually added from art project
+    "Bright Green", // manually added from art project
+    "Dark Turquoise", // manually added from dots
+    "Yellowish Green", // manually added from dots
+    "Medium Lavender",
+    "Cool Yellow",
+    "Spring Yellowish Green",
+    "Light Purple",
+    "Bright Yellowish Green",
+    "Sand Blue",
+    "Sand Yellow",
+    "Dark Orange",
+    "Dark Azur",
+    "Flame Yellowish Orange",
+    "Bright Purple",
+    "Vibrant Coral",
+    "Bright Reddish Violet",
+    "Bright Bluish Green",
+    "Bright Orange",
+    "Bright Blue",
+    "Dark Stone Grey",
+    "Bright Yellow",
+    "Bright Red",
+    "Brick Yellow",
+    "Earth Blue",
+    "Medium Stone Grey",*/
+
+    "Medium Lilac",
+    "Medium Lavender",
+    "Dark Green",
+    "Vibrant Yellow",
+    "Naugat",
+    "Cool Yellow",
+    "Spring Yellowish Green",
+    "Light Purple",
+    "Bright Yellowish Green",
+    "Sand Blue",
+    "Sand Yellow",
+    "Dark Orange",
+    "Dark Azur",
+    "Flame Yellowish Orange",
+    "Bright Purple",
+    "Lavender",
+    "Aqua",
+    "Vibrant Coral",
+    "Bright Green",
+    "Medium Azure",
+    "Bright Reddish Violet",
+    "Dark Brown",
+    "Bright Bluish Green",
+    "Bright Orange",
+    "Bright Blue",
+    "Light Nougat",
+    "Dark Stone Grey",
+    "Medium Nougat",
+    "Reddish Brown",
+    "Dark Red",
+    "Bright Yellow",
+    "Bright Red",
+    "Brick Yellow",
+    "White",
+    "Black",
+    "Olive Green",
+    "Earth Blue",
+    "Medium Stone Grey",
+    "Earth Green",
+];
+
+const BRICKLINK_STUD_COLORS = ALL_BRICKLINK_SOLID_COLORS.filter((color) =>
+    KNOWN_BRICKLINK_STUD_COLOR_NAMES.includes(color.name)
+).sort((a, b) => {
+    return a.name > b.name ? 1 : -1;
+});
+
+const BRICKLINK_TILE_COLORS = ALL_BRICKLINK_SOLID_COLORS.filter((color) =>
+    KNOWN_BRICKLINK_TILE_COLOR_NAMES.includes(color.name)
+).sort((a, b) => {
+    return a.name > b.name ? 1 : -1;
+});
+
+const ADDITIONAL_COLORS = ["Very Light Gray"];
+let ALL_VALID_BRICKLINK_COLORS = ALL_BRICKLINK_SOLID_COLORS.sort((a, b) => {
+    return a.name > b.name ? 1 : -1;
+});
+// .filter(
+//     color =>
+//         KNOWN_BRICKLINK_STUD_COLOR_NAMES.includes(color.name) ||
+//         KNOWN_BRICKLINK_TILE_COLOR_NAMES.includes(color.name) ||
+//         ADDITIONAL_COLORS.includes(color.name)
+// ).sort((a, b) => {
+//     return a.name > b.name ? 1 : -1;
+// });
+
+const PIXEL_TYPE_OPTIONS = [
+    {
+        name: "1x1 Round Tile",
+        number: 98138,
+    },
+    {
+        name: "1x1 Round Plate",
+        number: 4073,
+    },
+    {
+        name: "1x1 Square Tile",
+        number: "3070b",
+    },
+    {
+        name: "1x1 Square Plate",
+        number: 3024,
+    },
+    {
+        name: "1x1 Square Brick",
+        number: 3005,
+    },
+    {
+        name: "Variable Tile",
+        number: "variable_tile",
+    },
+    {
+        name: "Variable Plate",
+        number: "variable_plate",
+    },
+    {
+        name: "Variable Brick",
+        number: "variable_brick",
+    },
+];
+
+// use this for instructions - we prioritize readability over accuracy here
+const PIXEL_TYPE_TO_FLATTENED = {
+    98138: 98138,
+    4073: 98138,
+    3024: "3070b",
+    "3070b": "3070b",
+    3005: "3070b",
+    variable_tile: "variable_tile",
+    variable_plate: "variable_tile",
+    variable_brick: "variable_tile",
+};
